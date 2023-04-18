a437423e4e13f0cbb5f7b61abb5d16b0ef758fd9
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2023-04-18 17:30:59 -0400

    adiciona o icon-linux no canto superior direito

--- js/index.js
+++ js/index.js
@@ -11,13 +11,3 @@ function linkedinAcess() {
     open("https://www.linkedin.com/in/gabriel-moitinho-de-albuquerque-883b281a0/")
 }
 
-
-function scrollTo() {
-    $(".accordion-body").on("shown.bs.collapse") {
-        var selected = $(this);
-        var collapseh = $(".collapse.in").height();
-        $.scrollTo(selected, 500, {
-            offset: -(collapseh)
-        });
-    }; 
-}
