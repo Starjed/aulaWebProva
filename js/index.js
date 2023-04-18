83cfb43e6f249cfcd3bbf4283dbfeca9a941732b
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2023-04-18 16:17:37 -0400

    Adiciona animação de fundo

--- js/index.js
+++ js/index.js
@@ -10,3 +10,14 @@ function gitAcess() {
 function linkedinAcess() {
     open("https://www.linkedin.com/in/gabriel-moitinho-de-albuquerque-883b281a0/")
 }
+
+
+function scrollTo() {
+    $(".accordion-body").on("shown.bs.collapse") {
+        var selected = $(this);
+        var collapseh = $(".collapse.in").height();
+        $.scrollTo(selected, 500, {
+            offset: -(collapseh)
+        });
+    }; 
+}
